cd42127bd8f8dd5f5864f79f5debf2f66e686125
brekkeberg <brekke.berg@gmail.com>
2023-09-15 14:12:53 -0700

    updated with gui, still needs some improvements

--- rps.js
+++ rps.js
@@ -1,9 +1,20 @@
 
+//creates js object for each button option by the button's id
+const buttonRock = document.querySelector('button#rock');
+const buttonPaper = document.querySelector('button#paper');
+const buttonScissors = document.querySelector('button#scissors');
+
+// runs function player's choice function upon button click
+buttonRock.addEventListener('click', game)
+buttonPaper.addEventListener('click', game)
+buttonScissors.addEventListener('click', game)
+
+
 let tieScore = 0;
 let computerScore = 0;
 let playerScore = 0;
-
 function game(){
+
     // random number between min and max
     function getRndInteger(min,max){
         return Math.floor(Math.random() * (max - min)) + min;
@@ -20,9 +31,14 @@ function game(){
             return "scissors"
         }
     }
-
-    let playerSelection = prompt("What is your choice?").toLowerCase();
+    
+    //delcares comp and user choices
+    let playerSelection = this.id;
     let computerSelection = getComputerChoice()
+
+    //makes new element in the html
+    
+    
 
     //plays one game of RPS
     function playRPSgame(){
@@ -56,16 +72,6 @@ function game(){
     console.log(playRPSgame())
     console.log("Computer Score: " + computerScore + ", Player Score: " + playerScore + ", Ties: " + tieScore)
 }
-
-for (let i = 0; i < 5; i++) {
-    console.log("Game Number " + (i+1))
-    game.call()
-} 
-
-console.log("Thank you for playing!")
-
-
-
 
 
 
